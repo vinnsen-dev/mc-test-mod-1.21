3d9b6926f9c487c130037622975f285b0101ee4e
Josh <jmconrathe@gmail.com>
2025-12-04 15:15:52 +0000

    Initial commit

---
+++ src/main/java/net/vinnsen/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.vinnsen.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
